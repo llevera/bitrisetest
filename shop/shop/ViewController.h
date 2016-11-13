d8dc8169837087eb61321824ffbe314ec0a3667f
Andrew Revell <andrew.r@markermetro.com>
2016-11-14 05:38:50 +1300

    Testing certs etc

---
+++ shop/shop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shop
+//
+//  Created by Andrew Revell on 14/11/16.
+//  Copyright © 2016 Andrew Revell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ shop/shop/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  shop
+//
+//  Created by Andrew Revell on 14/11/16.
+//  Copyright © 2016 Andrew Revell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
